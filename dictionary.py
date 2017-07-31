2f3953d3efb961a04641bf90fdee4375065c6405
Tsering Dolkar <14852697+tsering54@users.noreply.github.com>
2017-07-31 12:38:52 -0700

    Delete dictionary.py

--- dictionary.py
+++
@@ -1,5 +0,0 @@
-dictionary = {'name': 'Anna', 'age': '101', 'country of birth': 'The United States', 'favorite language': 'Python'}
-print 'My name is ' + dictionary['name']
-print 'My age is ' + dictionary['age']
-print 'My country of birth is ' , dictionary['country of birth']
-print 'My favorite language is ' , dictionary['favorite language']
